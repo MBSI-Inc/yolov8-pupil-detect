12a350659352629dc798ff50ec364d93617e9910
NKLam <khoalamvn@gmail.com>
2024-08-10 12:14:06 +1000

    Add gaze prediction with simple calibration
    Press C to calibrate

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import math
 from ultralytics import YOLO
 from sys import platform
 
+THRESHOLD_CENTER_OFFSET = 80
+
 def train():
     model = YOLO("yolov8m.pt")
     model.train(data="datasets/data.yaml", epochs=20)
@@ -41,10 +43,11 @@ def predict_webcam():
     class_name = ['Iris', 'R Eye', 'L Eye', 'Pupil'] 
     class_color = [(255, 0, 0), (0, 255, 0), (0, 255, 0), (0, 0, 255)]
 
-    box_len = 0
+    threshold_data = [0, 0, 0, 0] # Up Right Down Left
 
     while True:
         _, img = cam.read()
+        (frame_h, frame_w) = img.shape[:2] #w:image-width and h:image-height
         results = model(img, stream=True)
         for r in results: # results is a Generator Function
             best_iris_data = [0, 0, 0, 0, 0] # Format [x1, y1, x2, y2, confidence]
@@ -71,9 +74,42 @@ def predict_webcam():
             draw_box_data(img, x1, y1, x2, y2, confidence, "Iris", class_color[0])
             x1, y1, x2, y2, confidence = best_eye_data
             draw_box_data(img, x1, y1, x2, y2, confidence, "Eye", class_color[1])
-            
+
+            # Simple implementation for getting cailbrated threshold
+            # Press C on keyboard to record
+            x1, y1, x2, y2, confidence = best_iris_data
+            center_coord = [(x1 + x2) // 2, (y1 + y2) // 2]
+            # print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", threshold_data)
+            cv2.putText(img, "U", [frame_w // 2, threshold_data[0]], cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 100, 100), 1)
+            cv2.line(img, [0, threshold_data[0]], [frame_w, threshold_data[0]], (255, 100, 100), 1)
+
+            cv2.putText(img, "R", [threshold_data[1], frame_h // 2], cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 100, 100), 1)
+            cv2.line(img, [threshold_data[1], 0], [threshold_data[1], frame_h], (255, 100, 100), 1)
+
+            cv2.putText(img, "D", [frame_w // 2, threshold_data[2]], cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 100, 100), 1)
+            cv2.line(img, [0, threshold_data[2]], [frame_w, threshold_data[2]], (255, 100, 100), 1)
+
+            cv2.putText(img, "L", [threshold_data[3], frame_h // 2], cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 100, 100), 1)
+            cv2.line(img, [threshold_data[3], 0], [threshold_data[3], frame_h], (255, 100, 100), 1)
+
+            cv2.putText(img, "X", center_coord, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+            if cv2.waitKey(1) == ord('c'):
+                threshold_data[0] = center_coord[1] - THRESHOLD_CENTER_OFFSET # Up
+                threshold_data[1] = center_coord[0] - THRESHOLD_CENTER_OFFSET # Right
+                threshold_data[2] = center_coord[1] + THRESHOLD_CENTER_OFFSET # Down
+                threshold_data[3] = center_coord[0] + THRESHOLD_CENTER_OFFSET # Left
+
             # TODO: Predict gaze direction
-            cv2.putText(img, "right ", [50, 50], cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
+            if center_coord[0] < threshold_data[1]:
+                cv2.putText(img, "right", [50, 50], cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
+            elif center_coord[0] > threshold_data[3]:
+                cv2.putText(img, "left", [50, 50], cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
+            
+            if center_coord[1] < threshold_data[0]:
+                cv2.putText(img, "up", [200, 50], cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
+            elif center_coord[1] > threshold_data[2]:
+                cv2.putText(img, "down", [200, 50], cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
+
 
         cv2.imshow('Webcam', img)
         if cv2.waitKey(1) == ord('q'):
@@ -81,6 +117,7 @@ def predict_webcam():
 
     cam.release()
     cv2.destroyAllWindows()
+
 
 
 def draw_box_data(img, x1, y1, x2, y2, confidence, text, color):
